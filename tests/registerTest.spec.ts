bd4e221659aeb4f381fa600d809ba95b6ccd797e
AugustoRD <augusto06r@gmail.com>
2026-06-23 15:30:15 -0300

    feat: add test for account creation with balance in RegisterPage
    Co-authored-by: Joyce Maria <jojonobre1810@gmail.com>
    Co-authored-by: Marcos <marcospontodoc@gmail.com>

--- tests/registerTest.spec.ts
+++ tests/registerTest.spec.ts
@@ -20,6 +20,23 @@ test.describe('Create Account', () => {
 
     });
 
+    test('Create Account with Balance', async ({page}) => {
+        const registerPage = new RegisterPage(page);
+
+        await registerPage.gotoRegisterPage();
+        await registerPage.fillRegisterForm('teste2@exemplo.com', 'Nome Completo', '123456', '123456');
+         await registerPage.activeCreateWithBalance();
+        await registerPage.clickRegisterButton();
+           
+        
+        await expect(registerPage.modalText).toBeVisible();
+
+        await expect(registerPage.modalText).toHaveText(/foi criada com sucesso/);  
+         
+        await registerPage.closeModalButton.click();
+
+    });
+
 
 });
    
