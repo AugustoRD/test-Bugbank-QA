dc1e70f7922d06fe4367172654730f17bb7ebccc
AugustoRD <augusto06r@gmail.com>
2026-06-24 13:36:21 -0300

    refactor: streamline login tests by initializing pages in beforeEach
    Co-authored-by: Joyce Maria <jojonobre1810@gmail.com>
    Co-authored-by: Marcos <marcospontodoc@gmail.com>

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -3,89 +3,56 @@ import {LoginPage} from '../pages/LoginPage';
 import {RegisterPage} from '../pages/RegisterPage';
 
 test.describe('Login', () => {
-    test('Successful Login', async ({page}) => {
-        const loginPage = new LoginPage(page);
-        const registerPage = new RegisterPage(page);
+    let loginPage: LoginPage;
+    let registerPage: RegisterPage;
+
+    test.beforeEach(async ({page}) => {
+        loginPage = new LoginPage(page);
+        registerPage = new RegisterPage(page);
 
         //cadastra primeiro o usuário para depois logar
         await registerPage.gotoRegisterPage();
         await registerPage.fillRegisterForm('email@exemplo.com', 'Nome Completo', '123456', '123456');
         await registerPage.clickRegisterButton();
            
-        
         await expect(registerPage.modalText).toBeVisible();
   
         await registerPage.closeModalButton.click();
 
-        //login
         await loginPage.gotoLoginPage();
+    });
+
+
+    test('Successful Login', async ({page}) => {
+
+        //login
         await loginPage.login('email@exemplo.com', '123456');
         await expect(page).toHaveURL('https://bugbank.netlify.app/home');
 
-    
     });
     
-    test('Login with wrong password', async ({page}) => {
-        const loginPage = new LoginPage(page);
-        const registerPage = new RegisterPage(page);
-
-        await registerPage.gotoRegisterPage();
-        await registerPage.fillRegisterForm('email@exemplo.com', 'Nome Completo', '123456', '123456');
-        await registerPage.clickRegisterButton();
-           
-        
-        await expect(registerPage.modalText).toBeVisible();
-  
-        await registerPage.closeModalButton.click();        
+    test('Login with wrong password', async ({page}) => {     
 
         //login com senha errada
-        await loginPage.gotoLoginPage();
         await loginPage.login('email@exemplo.com', 'wrong');
         await expect(loginPage.modalText).toHaveText(/Tente novamente/);
         
         await loginPage.closeModalButton.click();
     });
 
-    test('Login with an unregistered email', async ({page}) => {
-        const loginPage = new LoginPage(page);
-        const registerPage = new RegisterPage(page);
-
-        await registerPage.gotoRegisterPage();
-        await registerPage.fillRegisterForm('email@exemplo.com', 'Nome Completo', '123456', '123456');
-        await registerPage.clickRegisterButton();
-           
-        
-        await expect(registerPage.modalText).toBeVisible();
-  
-        await registerPage.closeModalButton.click();        
+    test('Login with an unregistered email', async ({page}) => {    
 
         //login com email não cadastrado
-        await loginPage.gotoLoginPage();
         await loginPage.login('teste@exemplo.com', '123456');
         await expect(loginPage.modalText).toHaveText(/Tente novamente/);
         
         await loginPage.closeModalButton.click();
     });
 
-    test('Login with an invalid email', async ({page}) => {
-        const loginPage = new LoginPage(page);
-        const registerPage = new RegisterPage(page);
-
-        await registerPage.gotoRegisterPage();
-        await registerPage.fillRegisterForm('email@exemplo.com', 'Nome Completo', '123456', '123456');
-        await registerPage.clickRegisterButton();
-           
-        
-        await expect(registerPage.modalText).toBeVisible();
-  
-        await registerPage.closeModalButton.click();        
+    test('Login with an invalid email', async ({page}) => {    
 
         //login com email de formato inválido
-        await loginPage.gotoLoginPage();
         await loginPage.login('testexemplo.com', '123456');
-        await expect(loginPage.warningText).toHaveText(/E-mailFormato inválidoSenhaAcessarRegistrarConheça/);
-       
-       
+        await expect(loginPage.warningText).toHaveText(/E-mailFormato inválidoSenhaAcessarRegistrarConheça/); 
     });
-  
 });
